1fb422c906ffafb710562cbb09be6fba96414d92
Carter Weinberg <weinbergcarter@gmail.com>
2026-06-22 15:02:40 -0400

    Made some modules and exported some functions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import appleTart from "./assets/apple-tart.png";
+import { renderHome } from "./modules/homepage.js";
 
 
-
-const content = document.getElementById("content");
-
-const img = document.createElement("img");
-img.src = appleTart;
-img.alt = "An apple tart";
-
-const heading = document.createElement("h1");
-heading.textContent = "Welcome to Our Restaurant";
-
-const paragraph = document.createElement("p");
-paragraph.textContent =
-    "Experience culinary excellence at its finest. Our restaurant offers an exquisite dining experience with a carefully curated menu featuring the freshest ingredients and innovative dishes. From the warm ambiance to our exceptional service, every detail is designed to make your visit unforgettable. Join us for an evening of delicious food, fine beverages, and cherished memories with loved ones.";
-
-content.appendChild(img);
-content.appendChild(heading);
-content.appendChild(paragraph);
+renderHome();
---
+++ src/modules/homepage.js
@@ -0,0 +1,20 @@
+import appleTart from "../assets/apple-tart.png";
+
+export function renderHome() {
+    const content = document.getElementById("content");
+
+    const img = document.createElement("img");
+    img.src = appleTart;
+    img.alt = "An apple tart";
+
+    const heading = document.createElement("h1");
+    heading.textContent = "Welcome to Our Restaurant";
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent =
+        "Experience culinary excellence at its finest. Our restaurant offers an exquisite dining experience with a carefully curated menu featuring the freshest ingredients and innovative dishes. From the warm ambiance to our exceptional service, every detail is designed to make your visit unforgettable. Join us for an evening of delicious food, fine beverages, and cherished memories with loved ones.";
+
+    content.appendChild(img);
+    content.appendChild(heading);
+    content.appendChild(paragraph);
+}
